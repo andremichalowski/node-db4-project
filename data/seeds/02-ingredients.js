202cb848ac4e5f5f8850be88c2aa9af387667d0d
amichalo421 <andre.michalowski@gmail.com>
2020-11-08 10:29:25 -0500

    Add seeds

---
+++ data/seeds/02-ingredients.js
@@ -0,0 +1,16 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('table_name').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('ingredients').insert([
+        {id: 1, ingredient_name: 'butter'},
+        {id: 2, ingredient_name: 'milk'},
+        {id: 3, ingredient_name: 'sugar'},
+        {id: 4, ingredient_name: 'rice'},
+        {id: 5, ingredient_name: 'flour'},
+        {id: 6, ingredient_name: 'beans'},
+        {id: 7, ingredient_name: 'salt'},
+      ]);
+    });
+};
