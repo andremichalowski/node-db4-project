c3622620da75d7d73da868b1199dc120cf31501d
amichalo421 <andre.michalowski@gmail.com>
2020-11-08 10:33:02 -0500

    Update scheme-router

--- schemes/recipe-router.js
+++ schemes/recipe-router.js
@@ -1,21 +1,53 @@
 const express = require('express'); //import
 
-const Schemes = require('./recipe-model.js');
+const Recipes = require('./recipe-model.js');
 
-const router = express.Router(); //instantiate
-router.use(express.json()) //configure
+const router = express.Router();
+router.use(express.json()) 
 
 
 // CRUD ENDPOINTS WITH ROUTER GO HERE
 
-router.get('/routerTest', (req, res) => {
-  res.status(200).json({ router: "Test GET request from recipe-router"})
+// router.get('/routerTest', (req, res) => {
+//   res.status(200).json({ router: "Test GET request from recipe-router"})
+// })
+
+router.get('/', (req, res) => {
+  Recipes.getRecipes()
+  .then(recipes => {
+    console.log(recipes)
+    res.status(200).json(recipes)
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get recipes' });
+  });
 })
-// router.get('/', (req, res) => {...}
-// router.get('/:id', (req, res) => {...}
-// router.post('/', (req, res) => {...}
-// router.delete('/:id', (req, res) => {...}
-// router.put('/:id', (req, res) => {...}
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+
+  Recipes.getShoppingList(id)
+  .then(list => {
+    console.log(list)
+    res.status(200).json(list)
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get recipes' });
+  });
+})
+
+router.get('/:id/instructions', (req, res) => {
+  const id = req.params.id;
+
+  Recipes.getInstructions(id)
+  .then(instructions => {
+    console.log(instructions)
+    res.status(200).json(instructions)
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get recipes' });
+  });
+})
 
 module.exports = router; //export
 
